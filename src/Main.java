22f11286a5dbe96c53dd32db55f407597421f9d9
largado <alexandrohipolito@gmail.com>
2023-10-08 11:08:59 -0300

    ver 0.0

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// soma 2 números de ponto flutuante
+// 08/10/2023
+// capítulo 1 página 7
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner entrada1 = new Scanner(System.in);
+        Scanner entrada2 = new Scanner(System.in);
+
+        float A = entrada1.nextFloat();
+        float B = entrada2.nextFloat();
+
+        double C = A+B;
+
+        System.out.println("O resultado da soma é: " + C);
+
+    }
+}
