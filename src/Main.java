7b5668cd18651806a635fd6b1d81eec6fbf3886c
alexa <alexandrohipolito@gmail.com>
2024-07-27 12:29:14 -0300

    teste commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 // soma 2 números de ponto flutuante
 // 08/10/2023
 // capítulo 1 página 7
+// teste sincronização com visual studio code em 27/07/2024
+
 public class Main {
     public static void main(String[] args) {
 
